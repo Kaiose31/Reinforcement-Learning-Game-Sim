ee0f03f50e3a4b31dd3f8e9f5bb0d449f8d537e8
Kaiose31 <saurabhpahwa31@gmail.com>
2021-03-20 14:15:50 +0530

    added Checkpoint System

---
+++ unity/Model/Assets/Standard Assets/CrossPlatformInput/Scripts/CheckpointS.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+public class CheckpointS : MonoBehaviour
+{
+    private bool isColliding;
+    public TrackCheckpoints trackCheckpoints;
+    public GameObject Chassis;
+    public void Start() {
+        Chassis = GameObject.FindGameObjectWithTag("Car");
+        
+    }
+
+    public void Update() { }
+    
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (isColliding) return;
+        isColliding = true;
+        if (Chassis)
+        {
+            //Debug.Log("PASSED ");
+
+            trackCheckpoints.PlayerThroughCheckpoint(this);
+        }
+
+    }
+
+    public void SetTrackCheckpoints(TrackCheckpoints trackCheckpoints) {
+        this.trackCheckpoints = trackCheckpoints; 
+   
+    }
+
+
+}
+
+
+
---
+++ unity/Model/Assets/Standard Assets/CrossPlatformInput/Scripts/TrackCheckpoints.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackCheckpoints : MonoBehaviour
+{
+    private List<CheckpointS> checkpointSList;
+    private int nextCheckpointSIndex;
+    private void Awake() {
+        Transform checkpointsTransform = transform.Find("Checkpoints");
+        checkpointSList = new List<CheckpointS>();
+        foreach (Transform checkpointSingleTransform in checkpointsTransform) {
+            CheckpointS checkpointS = checkpointSingleTransform.GetComponent<CheckpointS>();
+            checkpointS.SetTrackCheckpoints(this);
+            checkpointSList.Add(checkpointS);
+        }
+        nextCheckpointSIndex = 0;
+    }
+
+    public void PlayerThroughCheckpoint(CheckpointS checkpointS) {
+        //  Debug.Log(checkpointS.transform.name);
+        if (checkpointSList.IndexOf(checkpointS) == nextCheckpointSIndex)
+        {
+            Debug.Log("Correct");
+            nextCheckpointSIndex = (nextCheckpointSIndex +1 )% checkpointSList.Count;
+
+        }
+        else {
+
+            Debug.Log("wrong");
+
+        }
+
+
+    }
+    
+}
